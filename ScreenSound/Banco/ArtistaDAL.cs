684ef1032828698b88cbc4375c32fa090ae419f1
Bruno Paese <brunoviniciuspaese@gmail.com>
2024-05-11 16:21:28 -0300

    refatoracao utilizando orm

--- ScreenSound/Banco/ArtistaDAL.cs
+++ ScreenSound/Banco/ArtistaDAL.cs
@@ -7,76 +7,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ScreenSound.Banco
+namespace ScreenSound.Banco;
+
+internal class ArtistaDAL
 {
-    internal class ArtistaDAL
+    private readonly ScreenSoundContext Context;
+
+    public ArtistaDAL(ScreenSoundContext context)
     {
-
-        public IEnumerable<Artista> Listar()
-        {
-            var lista = new List<Artista>();
-            string sql = "select * from Artistas";
-
-            using var connection = new Connection().ObterConexao();
-            connection.Open();
-            SqlCommand command = new SqlCommand(sql, connection);
-            using SqlDataReader dataReader = command.ExecuteReader();
-
-            while (dataReader.Read())
-            {
-                string nomeArtista = Convert.ToString(dataReader["Nome"]);
-                string bioArtista = Convert.ToString(dataReader["Bio"]);
-                int idArtista = Convert.ToInt32(dataReader["Id"]);
-
-                Artista artista = new Artista(nomeArtista, bioArtista) { Id = idArtista };
-
-                lista.Add(artista);
-            }
-
-            return lista;
-        }
-
-        public void Adicionar(Artista artista)
-        {
-            string sql = "insert into Artistas (Nome, FotoPerfil, Bio) values (@nome, @fotoPerfil, @bio)";
-
-            using var connection = new Connection().ObterConexao();
-            connection.Open();
-            SqlCommand command = new SqlCommand(sql, connection);
-            command.Parameters.AddWithValue("@nome", artista.Nome);
-            command.Parameters.AddWithValue("@fotoPerfil", artista.FotoPerfil);
-            command.Parameters.AddWithValue("@bio", artista.Bio);
-
-            int retorno = command.ExecuteNonQuery();
-            Console.WriteLine($"{retorno} linhas afetadas.");
-        }
-
-        public void Atualizar(Artista artista) 
-        {
-            string sql = "update Artistas set Nome = @nome, Bio = @bio where Id = @id";
-
-            using var connection = new Connection().ObterConexao();
-            connection.Open();
-            SqlCommand command = new SqlCommand(sql, connection);
-            command.Parameters.AddWithValue("@nome", artista.Nome);
-            command.Parameters.AddWithValue("@bio", artista.Bio);
-            command.Parameters.AddWithValue("@id", artista.Id);
-
-            int retorno = command.ExecuteNonQuery();
-            Console.WriteLine($"{retorno} linhas afetadas.");
-        }
-
-        public void Excluir(Artista artista)
-        {
-            string sql = "delete from Artistas where Id = @id";
-
-            using var connection = new Connection().ObterConexao();
-            connection.Open();
-            SqlCommand command = new SqlCommand(sql, connection);
-            command.Parameters.AddWithValue("id", artista.Id);
-
-            int retorno = command.ExecuteNonQuery();
-            Console.WriteLine($"{retorno} linhas afetadas.");
-        }
+        Context = context;
     }
+    public IEnumerable<Artista> Listar()
+    {
+        return Context.Artistas.ToList();
+    }
+
+    public void Adicionar(Artista artista)
+    {
+        Context.Artistas.Add(artista);
+        Context.SaveChanges();
+    }
+
+    public void Atualizar(Artista artista) 
+    {
+        Context.Artistas.Update(artista);
+        Context.SaveChanges();
+    }
+
+    public void Excluir(Artista artista)
+    {
+        Context.Artistas.Remove(artista);
+        Context.SaveChanges();
+    }
+
+    public Artista? RecuperarPeloNome(string nome)
+    {
+        return Context.Artistas.FirstOrDefault(a => a.Nome.Equals(nome));
+    }
+
 }
--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -3,31 +3,8 @@ using ScreenSound.Menus;
 using ScreenSound.Modelos;
 using System.Data;
 
-try
-{
-    var artitaDAL = new ArtistaDAL();
-
-    var listaArtistas = artitaDAL.Listar();
-
-    foreach (var artista in listaArtistas)
-    {
-        Console.WriteLine(artista);
-    }
-        
-}
-catch (Exception ex)
-{
-    Console.WriteLine(ex.Message);
-}
-
-return;
-
-Artista ira = new Artista("Ira!", "Banda Ira!");
-Artista beatles = new("The Beatles", "Banda The Beatles");
-
-Dictionary<string, Artista> artistasRegistrados = new();
-artistasRegistrados.Add(ira.Nome, ira);
-artistasRegistrados.Add(beatles.Nome, beatles);
+var context = new ScreenSoundContext();
+var artistaDAL = new ArtistaDAL(context);
 
 Dictionary<int, Menu> opcoes = new();
 opcoes.Add(1, new MenuRegistrarArtista());
@@ -66,7 +43,7 @@ void ExibirOpcoesDoMenu()
     if (opcoes.ContainsKey(opcaoEscolhidaNumerica))
     {
         Menu menuASerExibido = opcoes[opcaoEscolhidaNumerica];
-        menuASerExibido.Executar(artistasRegistrados);
+        menuASerExibido.Executar(artistaDAL);
         if (opcaoEscolhidaNumerica > 0) ExibirOpcoesDoMenu();
     } 
     else
